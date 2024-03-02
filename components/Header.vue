91fdb8ec25b2ed7c58af4345695dc2ddab66749c
Sagar Rana Magar <smagar816@gmail.com>
2024-03-02 21:23:43 +0545

    first commit

---
+++ components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <header>
+    <nav class="bg-gradient-to-r from--500 px-4 py-2.5">
+      <div class="grid justify-center items-center">
+        <span class="welcomeText">
+            Choose the Nation to Continue
+        </span>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400..700&display=swap');
+    .welcomeText {
+        font-size:2.5rem;
+        font-family: "Dancing Script", cursive;
+    }
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,18 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  ssr: false,
+  app: {
+    head: {
+      title: 'सिफारिश प्रमाणीकरण पोर्टल',
+    }
+  },
+
+  css: ['~/assets/css/main.css'],
+
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+})
